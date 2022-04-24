f700c0f964e95391f1649995c79395de85c4235c
Jason Schafer <jason59549@gmail.com>
2022-04-23 21:20:07 -0400

    Moved Code Entry Directly Back into Bin

--- lib/ruby_proctor.rb
+++ lib/ruby_proctor.rb
@@ -1,14 +1,16 @@
-$LOAD_PATH.unshift File.dirname($0)
+#$LOAD_PATH.unshift File.dirname($0)
 
 require 'rubygems'
 require 'bundler/setup' # IS unable to find os-1.1.4 on OCRA windows, lots of chicken/egg problems.
 
 require 'os'
 
-if OS.posix?
-  require 'ruby_proctor/interfaces/terminal.rb'
-  ruby_proctor_terminal()
-elsif OS.windows?
-  require 'ruby_proctor/interfaces/gui.rb'
-  ruby_proctor_gui()
+ruby_proctor()
+  if OS.posix?
+    require 'ruby_proctor/interfaces/terminal.rb'
+    ruby_proctor_terminal()
+  elsif OS.windows?
+    require 'ruby_proctor/interfaces/gui.rb'
+    ruby_proctor_gui()
+  end
 end

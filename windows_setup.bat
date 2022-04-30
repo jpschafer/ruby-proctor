66ced9bd71b3a448faad2dbdb116c787593b2933
Jason Schafer <jason59549@gmail.com>
2022-04-30 17:41:37 -0400

    Added Windows Batch Setup

--- windows_setup.bat
+++ windows_setup.bat
@@ -1,2 +1,2 @@
-pushd %~dp0
+SET "PATH=%PATH%;C:\Ruby30-x64\bin"
 ridk install 1 & gem install bundler -v 2.2.32 & gem install tk & gem install os & bundler install & gem build & gem install --local .\ruby_proctor-1.0.0.gem
